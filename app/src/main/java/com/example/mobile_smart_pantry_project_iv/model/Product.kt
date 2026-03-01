1617e8f8651e44c6fd3f5509ad1d9e893c7f8310
Maks <maksymilian0606@wp.pl>
2026-03-01 21:04:33 +0100

    dialog layout

--- app/src/main/java/com/example/mobile_smart_pantry_project_iv/model/Product.kt
+++ app/src/main/java/com/example/mobile_smart_pantry_project_iv/model/Product.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Product (
-    val uuid:Int,
+    val uuid:String,
     val name:String,
     val quantity: Int,
     val category: String,
